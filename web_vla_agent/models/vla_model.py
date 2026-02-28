ace330cff07ec0d02276232e31a41d8f867ea784
01fe23bci087 <simrankalkeri@gmail.com>
2026-03-01 00:19:55 +0530

    cache disabled

--- web_vla_agent/models/vla_model.py
+++ web_vla_agent/models/vla_model.py
@@ -350,6 +350,7 @@ class VLAModel:
                     # reference-counter cannot always collect promptly (especially
                     # with PEFT reference cycles).  A plain tuple is freed as
                     # soon as outputs is deleted.
+                    use_cache=False,
                     return_legacy_cache=True,
                 )
         finally:
